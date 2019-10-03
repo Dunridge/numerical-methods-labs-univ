b8842ac58e7e635fee8e8234e5e1347cc5a16f76
Maksym Polinka <Polinka7max@gmail.com>
2019-10-03 18:23:20 +0300

    remade and tested the dichotomy method for lab 1

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -12,6 +12,23 @@ def not_yet_implemented():
     print("not yet implemented...")
 
 
+def input_matrix(n):  # lab2
+    print("enter the matrix of coefficients: ")
+    A = np.zeros((n, n))
+    for i in range(n):
+        A[i] = input().split(" ")
+    return A
+    # how to add add elements to a matrix that is not predefined by the user
+
+
+def input_b_coefficients(n):  # lab2
+    print("enter the b coefficients: ")
+    b = np.zeros((1, n))
+    for i in range(n):
+        b[i] = input()
+    return b
+
+
 def choose_solution_method(chosen_method):
     if chosen_method == 1:
         # enter the parameters a, b, eps here
@@ -21,7 +38,12 @@ def choose_solution_method(chosen_method):
         dichotomy_method(a, b, eps)
         return
     if chosen_method == 2:
-        relaxation_method()
+        # enter parameters n, A, b, eps
+        n = int(input("enter the dimensions of the equation: "))
+        A = input_matrix(n)  # here you have an error in the algorithm: you don't have an n x n matrix for the equation
+        b = input_b_coefficients(n)
+        eps = input("enter epsilon: ")
+        relaxation_method(n, A, b, eps)
         return
     else:
         incorrect_method_number()
@@ -33,28 +55,59 @@ def variant_function(x):
 
 def dichotomy_method(a, b, eps):
     # (варіант 7: 3*х + cos(x) + 1 = 0)
-    print("dichotomy method, entered values are: a -", a, " b -", b, " eps -", eps)
-    d = eps / 2
-    x1 = (a + b - d) / 2
-    x2 = (a + b + d) / 2
-    f1 = variant_function(x1)
-    f2 = variant_function(x2)
-    while abs(b - a) < eps:
-        if f1 >= f2:
-            b = x2
+    while abs(b - a) > eps:
+        x = (a + b) / 2.0
+        fx = variant_function(x)
+        fa = variant_function(a)
+        if (fx < 0 and fa < 0) or (fx > 0 and fa > 0):
+            a = x
         else:
-            a = x1
-        x1 = (a + b - d) / 2
-        x2 = (a + b + d) / 2
-        f1 = variant_function(x1)
-        f2 = variant_function(x2)
-
-    x_min = (a + b) / 2
-    print("the minimal x is ", x_min)
+            b = x
+    print("the minimal x is ", x)
 
 
-def relaxation_method():
+def relaxation_method(n, A, b, eps):
+    print("relaxation method: entered parameters: ", n, A, b, eps)
+    # матеріали для n-вимірного випадку:
+    # http://www.mathros.net.ua/vykorystannja-metodu-relaksacii-dlja-znahodzhennja-rozvjazku-slar.html
     # метод релаксації (алгоритм + приклад) можна знайти
     # в зошиті для ЧМ на зеленій закладці
-    print("relaxation method")
-    return
+    # на початку треба ввести кількість змінних, матрицю коефіцієнтів А
+    # b - вільні коефіцієнти, eps - точність
+
+    b1 = []
+    A1 = [[]]
+    Rp = []
+
+    for i in range(1, n):
+        for j in range(1, j):
+            A1[i, j] = - A[i, j] / A[i, i]
+        b1[i] = b[i] / A[i, i]
+    X = 0
+    Rn = 0
+    for i in range(1, n):
+        S = 0
+        for j in range(1, n):
+            if i < j:
+                S = S + A[i, j] * X[j]
+            Rp[i] = b1[i] - X[i] + S
+
+    while k != n:
+        maxR = -999
+        for i in range(1, n):
+            if (abs(Rp[i]) > maxR):
+                maxR = abs(Rp[i])
+                maxIndex = i
+        for i in range(1, n):
+            if i < maxIndex:
+                Rn[i] = Rp[j] + A1[i, maxIndex] * Rp[maxIndex]
+            if i > maxIndex:
+                Rn[i] = 0
+                X[maxIndex] = X[maxIndex] + Rp[maxIndex]
+            k = 0
+        for i in range(1, n):
+            if abs(Rn[i] < eps):
+                k = k + 1
+            Rp[i] = Rn[i]
+
+    print("x is ", X)
